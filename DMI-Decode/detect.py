ed583135cd2734d8329a3358501bc684ac5a8da5
venkatesh <vm_b18cse023_3@vmb18cse0233.localdomain>
2021-11-03 16:36:01 +0530

    dmicode working xd

--- DMI-Decode/detect.py
+++ DMI-Decode/detect.py
@@ -1,3 +1,20 @@
 from dmidecode import DMIDecode
 dmi= DMIDecode()
-print('Manufacturer:\t', dmi.manufacturer())
+
+def detect_vmware():
+    manufacturer= dmi.manufacturer().lower()
+    model = dmi.model().lower()
+    serial_number = dmi.serial_number().lower()
+    vmware = 'vmware'
+    return  ( manufacturer.find(vmware)+1) or ( serial_number.find(vmware)+1) or ( model.find(vmware)+1 )
+
+def detect_virtual_box():
+    return false
+
+def detect():
+   return  detect_vmware() or detect_virtual_box()
+
+if detect():
+    print("me vm uwu")
+else:
+    print("me no vm :/")

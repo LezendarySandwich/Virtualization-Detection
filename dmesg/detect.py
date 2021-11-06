31928f20ecc9ee0de2b3cb00427ae689a3e5f84f
tanmay2625 <53395063+tanmay2625@users.noreply.github.com>
2021-11-06 13:18:36 +0530

    Update detect.py

--- dmesg/detect.py
+++ dmesg/detect.py
@@ -1,13 +1,8 @@
 import os
-
-def detect_vmware():
-    os.system("dmesg | grep virtual | grep Detected >tmp.txt")
+    
+def detect():
+    os.system("dmesg | grep virtual >tmp.txt")
     is_vm = os.stat("tmp.txt").st_size != 0
     os.system("rm tmp.txt")
     return is_vm
-    
-    
-def detect():
-    if detect_vmware():return 1
-    return 0
     
